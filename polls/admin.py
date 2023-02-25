7cfaf6a74ec787b54dd657a23ca23711c1a63ffa
unknown <urmatcholponbaev055@gmail/com>
2023-02-25 13:36:12 +0300

    Apple

---
+++ polls/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    fieldsets = [
+        ('Биздин суроолор:', {'fields': ['question_text']}),
+        ('Даталар:', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice) 
